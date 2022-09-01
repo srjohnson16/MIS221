eb425b3bc258c20160efd935b607dc6874ced754
srjohnson16 <srjohnson16@crimson.ua.edu>
2022-09-01 01:16:19 +0100

    added new file: 'string name.cs'

---
+++ string name.cs
@@ -0,0 +1,16 @@
+string name= "";
+int age =0;
+
+System.Console.WriteLine("Please enter your name");
+age = int.Parse(Console.ReadLine());
+
+System.Console.WriteLine("Please enter your name");
+name = int.Parse(Console.ReadLine());
+
+
+if(age>= 16) 
+{
+
+}
+source.GetLine(name);
+
